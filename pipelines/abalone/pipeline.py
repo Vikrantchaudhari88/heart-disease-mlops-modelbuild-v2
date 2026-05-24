34d52edd2e9d3a74f86cca135cda7976937f1a10
Vikrantchaudhari88 <vikrant.chaudhari88@gmail.com>
2026-05-25 02:36:53 +0530

    Update pipeline.py
    in steps_args = model.register(
    inference_instances =[ "ml.t3.medium"]) is exchanged with ml.m5.large

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -288,8 +288,8 @@ def get_pipeline(
     step_args = model.register(
         content_types=["text/csv"],
         response_types=["text/csv"],
-        inference_instances=["ml.t2.medium", "ml.t3.medium"],
-        transform_instances=["ml.t3.medium"],
+        inference_instances=["ml.t2.medium", "ml.m5.large"],
+        transform_instances=["ml.m5.large"],
         model_package_group_name=model_package_group_name,
         approval_status=model_approval_status,
         model_metrics=model_metrics,
